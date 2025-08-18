8dd1c2aa18762097653bcec1da1b518200ec6aaf
KRISH <addresskrish@gmail.com>
2025-08-18 23:32:18 +0530

    updated

--- 01-first-next-app/app/page.tsx
+++ 01-first-next-app/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold mb-4">My Basic Next.js App</h1>
+      <p className="text-lg text-gray-700">Welcome! This is a simple homepage built with Next.js and TypeScript.</p>
+    </main>
   );
 }
